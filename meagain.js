737815e92c17e903aece72a041722d255aaa1be2
Utsav Singh <utsav0111@gmail.com>
2025-12-19 04:53:22 +0530

    filepush.js

--- meagain.js
+++ meagain.js
@@ -2,3 +2,6 @@ console.log("me again!");
 console.log("try modifying :");
 console.log("testing differ in git");
 console.log("failed attempt");
+console.log("trying to stagged");
+console.log("selfone");
+console.log("going back after commiting");
